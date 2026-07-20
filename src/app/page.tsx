5cbe16c6f23c8faee0e3f8ec1fa84f178376ef44
cahayailmukrw <cahayailmukrw@gmail.com>
2026-07-20 08:25:29 +0000

    Use logo.png as app logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center gap-3">
-              <img
+              <Image
                 src="/logo.png"
                 alt="SMPIT Cahaya Ilmu Logo"
                 width={50}
